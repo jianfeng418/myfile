a2b688380b8001e98f1f509323a493132497db1c
jianfeng418 <jianfeng418@sina.com>
2017-04-07 14:42:41 +0800

    Add files via upload

---
+++ router.js
@@ -0,0 +1,9 @@
+function route(handle,pathname){
+	console.log("About to a request for "+pathname);
+	if(typeof handle[pathname] === 'function'){
+		handle[pathname]();
+	}else{
+		console.log("no request handler found for "+pathname);
+	}
+}
+exports.route = route;
